3207682c2064904fc4e91b9f2688093e898deb9d
Solomka <76917656+Ingur-5967@users.noreply.github.com>
2025-03-25 22:46:24 +0300

    Delete SortAlg/alg directory

--- SortAlg/alg/time_util.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def time_execute(func):
-    def function(*args):
-        start = time.time()
-        result = func(*args)
-        return result, time.time() - start
-    return function
